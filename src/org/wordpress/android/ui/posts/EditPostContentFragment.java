27a6ea1cab1584e1b7159175a0c244ea7f8e13f2
petersonca <peterson.ca@gmail.com>
2014-07-10 18:24:27 -0500

    Default title to today's date.

--- src/org/wordpress/android/ui/posts/EditPostContentFragment.java
+++ src/org/wordpress/android/ui/posts/EditPostContentFragment.java
@@ -89,7 +89,9 @@ import org.xmlrpc.android.ApiHelper;
 
 import java.io.File;
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Vector;
 
@@ -205,6 +207,12 @@ public class EditPostContentFragment extends Fragment implements TextWatcher,
             }
             if (!TextUtils.isEmpty(post.getTitle())) {
                 mTitleEditText.setText(post.getTitle());
+            }
+            else {
+                Calendar c = Calendar.getInstance();
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd (EEE)");
+                String defaultTitle = dateFormat.format(c.getTime());
+                mTitleEditText.setText(defaultTitle);
             }
 
             postSettingsButton.setText(post.isPage() ? R.string.page_settings : R.string.post_settings);
